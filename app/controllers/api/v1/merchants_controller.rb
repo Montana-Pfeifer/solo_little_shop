f146ea8791bbde98bac15b558ad955141c7d90b1
Matt Haefling <m.haefling@protonmail.com>
2025-01-07 11:25:15 -0700

    Merchant Create new Merchant - Working

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -10,6 +10,11 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.format_merchant_json(merchant)
   end
 
+  def create
+    new_merchant = Merchant.create(merchant_params)
+    render json: MerchantSerializer.format_merchant_json(new_merchant)
+  end
+
   def merchant_params
     params.require(:merchant).permit(:name)
   end
