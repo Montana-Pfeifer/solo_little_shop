50bc96fb5783cb5c5671fba59e7b8ac2854b8af3
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-01-07 12:28:01 -0600

    item validations created

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :unit_price, presence: true, numerically: {only_float: true}
+  validates :merchnat_id, presence: true, numerically: {only_integer: true}
 end
