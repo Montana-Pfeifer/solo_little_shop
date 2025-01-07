2064dfca8b86bcdd444c3fa7c6fcd67cc573258c
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-01-07 14:22:35 -0700

    little tests in models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,19 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe "relationships" do
-    it { should have_many :invoices }
+
+    it "validates customer" do
+      customer = Customer.create!(first_name: "John", last_name: "Smith")
+
+      expect(customer.first_name?).to be(true)
+      expect(customer.last_name?).to be(true)
+
+    end
+    
+    it "shows an error when customer isn't valid" do
+      customer = Customer.create!(first_name: "", last_name: "Smith")
+
+      expect(customer.first_name?).to be(false)
+    end
   end
 end
