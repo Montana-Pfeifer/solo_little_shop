03b99b5ee07d2e5a44fb62e455e78fdd53f9a5f9
Matt Haefling <m.haefling@protonmail.com>
2025-01-07 20:27:25 -0700

    Merchant endpoint fetch all query selectors, sorted, count, status, delete, request tests added - all tests are passing

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,7 +1,7 @@
 class MerchantSerializer
   def self.format_merchant(merchant)
     {
-      id: merchant.id,
+      id: merchant.id.to_s,
       type: "merchant",
       attributes: {
         name: merchant.name
@@ -9,17 +9,31 @@ class MerchantSerializer
     }
   end
 
+  def self.format_merchant_json(merchant)
+    { data:
+      format_merchant(merchant)
+    }
+  end
+
   def self.format_merchants_json(merchants)
     { data:
-      merchants.map do |merchant|
-        format_merchant(merchant)
+    merchants.map do |merchant|
+      format_merchant(merchant)
       end
     }
   end
 
-  def self.format_merchant_json(merchant)
+  def self.format_merchants_count_json(merchants)
     { data:
-      format_merchant(merchant)
+    merchants.map do |merchant|
+      {
+      id: merchant.id.to_s,
+      type: "merchant",
+      attributes: {
+        name: merchant.name,
+        item_count: merchant.items.count
+      }}
+    end
     }
   end
 end
