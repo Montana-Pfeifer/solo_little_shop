9414789175dea27290c3c90b028009e4953d4d04
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-01-08 02:07:39 -0700

    removed useless code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,18 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :bad_request_parse_error
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
-  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :bad_request_parse_error
+  
 
   private
-  def missing_merchant_params?
-    params[:merchant].nil? || params[:merchant][:name].blank?
-  end
-
-  def missing_item_params?
-    params[:item].nil? || params[:item][:name].blank? || params[:item][:description].blank? || params[:item][:unit_price].blank? || params[:item][:merchant_id].blank?
-  end
-
+  
   def record_not_found(exception)
     render json: ErrorSerializer.format_error(404, exception.message, "Record Not Found"), status: :not_found
   end
 
-  def record_invalid(exception)
-    render json: ErrorSerializer.format_error(422, exception.record.errors.full_messages.join(", "), "Validation Error"), status: :unprocessable_entity
-  end
-
   def parameter_missing(exception)
     render json: ErrorSerializer.format_error(400, "Missing required parameter: #{exception.param}", "Bad Request"), status: :bad_request
-  end
-
-  def render_bad_request(exception)
-    render json: ErrorSerializer.format_error(400, exception, "Bad Request"), status: :bad_request
   end
 
   def bad_request_parse_error(exception)
