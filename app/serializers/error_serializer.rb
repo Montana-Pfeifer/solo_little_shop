a118e2f84364ab3d5dc3d3f7536819e8b8c9def5
Matt Haefling <m.haefling@protonmail.com>
2025-01-14 12:14:51 -0700

    Corrected Error Serializer to confirm its still converting status to string

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -4,7 +4,7 @@ class ErrorSerializer
       message: "There were errors processing your request",
       errors: errors.map do |error|
         {
-          status: error[:status],
+          status: error[:status].to_s,
           title: error[:title],
           detail: error[:detail]
         }
