aebb75c9ac03616d0209d0927afc9ed00e665922
Matt Haefling <m.haefling@protonmail.com>
2025-01-14 19:04:17 -0700

    Updated Readme, Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,29 +8,30 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get "/api/v1/merchants",            to: "api/v1/merchants#index"
-  get '/api/v1/merchants/find',       to: 'api/v1/merchants#find'
-  get "/api/v1/merchants/:id",        to: "api/v1/merchants#show"
-  post "/api/v1/merchants",           to: "api/v1/merchants#create"
-  patch "/api/v1/merchants/:id",      to: "api/v1/merchants#update"
-  delete "/api/v1/merchants/:id",     to: "api/v1/merchants#destroy"
-
+  namespace :api do
+    namespace :v1 do
+      get "/merchants",            to: "merchants#index"
+      get "/merchants/find",       to: "merchants#find"
+      get "/merchants/:id",        to: "merchants#show"
+      post "/merchants",           to: "merchants#create"
+      patch "/merchants/:id",      to: "merchants#update"
+      delete "/merchants/:id",     to: "merchants#destroy"
+    end
+  end
   
-  get "/api/v1/merchants/:id/items",  to: "api/v1/merchants_items#index"
-
-  get '/api/v1/items',                to: 'api/v1/items#index'
-  get '/api/v1/items/find_all',       to: 'api/v1/items#find_all'
-  get "/api/v1/merchants/:id/items",  to: "api/v1/merchants_items#index"
-  get "/api/v1/items/:id/merchant", to: "api/v1/items_merchant#index"
-
+  get "/api/v1/merchants/:id/items",              to: "api/v1/merchants_items#index"
+  get '/api/v1/items/find_all',                   to: 'api/v1/items#find_all'
+  get "/api/v1/items/:id/merchant",               to: "api/v1/items_merchant#index"
   get "/api/v1/merchants/:merchant_id/customers", to: "api/v1/customers#index"
-  get "/api/v1/merchants/:merchant_id/invoices", to: "api/v1/invoices#index"
+  get "/api/v1/merchants/:merchant_id/invoices",  to: "api/v1/invoices#index"
 
-  get '/api/v1/items',                to: 'api/v1/items#index'
-  get '/api/v1/items/:id',            to: 'api/v1/items#show'
-  post 'api/v1/items',                to: 'api/v1/items#create'
-  put 'api/v1/items/:id',           to: 'api/v1/items#update'
-  delete 'api/v1/items/:id',          to: 'api/v1/items#destroy'
-
-
+  namespace :api do
+    namespace :v1 do
+      get "/items",                 to: "items#index"
+      get "/items/:id",             to: "items#show"
+      post "/items",                to: "items#create"
+      put "/items/:id",             to: "items#update"
+      delete "/items/:id",          to: "items#destroy"
+    end
+  end
 end
