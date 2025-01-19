901f14e892713ddc7735c716d58f9410f1d41482
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-01-19 14:39:36 -0700

    validations on 5 coupons and coupons unique attributes

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,16 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
-  
+  has_many :invoices
+
   validates :name, :code, :discount_type, :value, presence: true
-  validates :code, uniqueness: true
+  validates :code, uniqueness: { scope: :merchant_id, message: "has already been taken for this merchant" }
+  validate :merchant_cannot_have_more_than_five_coupons, on: :create
+
+  private
+
+  def merchant_cannot_have_more_than_five_coupons
+    if merchant && merchant.has_reached_coupon_limit?
+      errors.add(:merchant, "cannot have more than 5 coupons")
+    end
+  end
 end
