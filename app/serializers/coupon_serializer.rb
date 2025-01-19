aeeac01b9cb00d2fe1e9bcceb203d695fddfaf92
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-01-19 14:40:51 -0700

    WET (possibly) added format for a single coupon

--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -13,10 +13,29 @@ class CouponSerializer
             discount_type: coupon.discount_type,
             value: coupon.value,
             status: coupon.status,
-            merchant: coupon.merchant_id.to_s
+            merchant: coupon.merchant_id.to_s,
+            usage_count: coupon.invoices.count.to_s
           }
         }
       end
+    }
+  end
+
+  def self.format_coupon(coupon)
+    {
+      data: {
+        id: coupon.id.to_s,
+        type: "coupon",
+        attributes: {
+          name: coupon.name,
+          code: coupon.code,
+          discount_type: coupon.discount_type,
+          value: coupon.value,
+          status: coupon.status,
+          merchant_id: coupon.merchant_id.to_s,
+          usage_count: coupon.invoices.count.to_s
+        }
+      }
     }
   end
 end
