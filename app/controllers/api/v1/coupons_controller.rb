ad431efc17e7c82f28c616bba40f83e6f89971b0
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-01-19 14:39:03 -0700

    testing deifferent methods refactored to project spec

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,7 +1,36 @@
 class Api::V1::CouponsController < ApplicationController
-  def index
-    coupons = Coupon.all
-    
-    render json: CouponSerializer.format_coupons(coupons)
+  class Api::V1::CouponsController < ApplicationController
+    def index
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      coupons = merchant.coupons
+      render json: CouponSerializer.format_coupons(coupons)
+    end
   end
+  
+
+  def show
+    coupon = Coupon.find(params[:id])
+    render json: CouponSerializer.format_coupon(coupon)
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    new_coupon = merchant.coupons.create(coupon_params)
+
+  
+    if new_coupon.persisted?
+      render json: CouponSerializer.format_coupon(new_coupon), status: :created
+    else
+      render json: { error: new_coupon.errors.full_messages }, status: :unprocessable_entity
+    end
+   
+  end
+
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :discount_type, :value, :status)
+  end
+  
 end
