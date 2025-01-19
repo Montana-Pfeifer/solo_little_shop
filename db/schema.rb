5b177bedb526232dc79664cd9bd4eeda69183ce0
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-01-19 14:42:07 -0700

    added migrations after reset of DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_18_172356) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_19_213636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_172356) do
     t.bigint "merchant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["code"], name: "index_coupons_on_code", unique: true
+    t.index ["code", "merchant_id"], name: "index_coupons_on_code_and_merchant_id", unique: true
     t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_172356) do
     t.string "status"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.integer "coupon_id"
+    t.index ["coupon_id"], name: "index_invoices_on_coupon_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
@@ -84,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_172356) do
   add_foreign_key "coupons", "merchants"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
+  add_foreign_key "invoices", "coupons"
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
